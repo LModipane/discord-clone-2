ab9fe86f4b5a6943f4d562b459e0487eafa1a050
shaun <modipanesh@gmail.com>
2023-10-05 12:10:30 +0200

    fix 1

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -5,7 +5,6 @@ declare global {
 	var prisma: PrismaClient | undefined;
 }
 
-const db = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db;
+const db = new PrismaClient();
 
 export default db;
