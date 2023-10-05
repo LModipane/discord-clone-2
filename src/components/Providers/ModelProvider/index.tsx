afe1cb6b7eb4b2d7586484109eee6967ac2f0a90
shaun <modipanesh@gmail.com>
2023-10-05 12:49:04 +0200

    fix 5

--- src/components/Providers/ModelProvider/index.tsx
+++ src/components/Providers/ModelProvider/index.tsx
@@ -1,15 +1,14 @@
 'use client';
-import {
-	CreateChannelModel,
-	CreateServerModel,
-	DeleteChannelModel,
-	DeleteServerModel,
-	EditChannelModel,
-	EditMembersModel,
-	EditServerModel,
-	InvitePersonModel,
-	LeaveServerModel,
-} from '@/components/Models';
+
+import CreateChannelModel from '@/components/Models/createChannelModel';
+import CreateServerModel from '@/components/Models/createServerModel';
+import DeleteChannelModel from '@/components/Models/deleteChannelModel';
+import DeleteServerModel from '@/components/Models/deleteServerModel';
+import LeaveServerModel from '@/components/Models/deleteServerModel';
+import EditChannelModel from '@/components/Models/editChannelModel';
+import EditMembersModel from '@/components/Models/editMembersModel';
+import EditServerModel from '@/components/Models/editServerModel';
+import InvitePersonModel from '@/components/Models/invitePersonModel';
 
 const ModelProvider = () => {
 	return (
